83e70b12f8d79747a098922e7cd9fdb0c4f45735
zhenmichael <112977307+zhenmichael@users.noreply.github.com>
2022-11-02 16:05:31 -0700

    wip

---
+++ tools/pipelines/helper/testFunction.mjs
@@ -0,0 +1,16 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const testConsoleFunction = () => {
+
+    let testText = "hello world!"
+
+    console.log(testText);
+
+    return testText;
+}
+
+
+testConsoleFunction();
