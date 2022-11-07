e4fac1efc56a7b39e09fcd5fe2c279c5ae6d6e93
zhenmichael <112977307+zhenmichael@users.noreply.github.com>
2022-11-07 11:09:58 -0800

    wip

--- tools/helper/test.py
+++ tools/helper/test.py
@@ -1,7 +1,13 @@
-# import xml.etree.ElementTree as ET
-# tree = ET.parse('tools/helper/mocha-junit-report.xml')
-# root = tree.getroot()
+import xml.etree.ElementTree as ET
+tree = ET.parse('mocha-junit-report.xml')
+root = tree.getroot()
 
-# print (root)
+print (root)
 
-print ('hello world')
+# create empty list for news items
+newsitems = []
+
+# iterate news items
+for failure in root.iter('failure'):
+    print(root)
+    print (failure.text)
