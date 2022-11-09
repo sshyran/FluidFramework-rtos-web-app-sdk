8e06f7171dda31f297efb598ddc978908fbbdde8
zhenmichael <112977307+zhenmichael@users.noreply.github.com>
2022-11-08 16:28:23 -0800

    wip

---
+++ tools/helper/testFunction.js
@@ -0,0 +1,39 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import * as fs from "fs";
+import * as parser from "xml2js";
+
+// var filename = process.argv.slice(2)[0];
+var filename = "./mocha-junit-report.xml"
+
+const parseTestReport = () => {
+    fs.readFile(filename,  'utf8', (err, data) => {
+        let failedTests;
+        parser.parseString(data, { mergeAttrs: true }, (err, res) => {
+            failedTests = findFailedTests(res);
+        })
+
+        failedTests?.forEach((test) => {
+            console.log(test.name[0])
+            console.log(test.failure[0]);
+        });
+
+        return failedTests;
+    });
+}
+
+const findFailedTests = (obj) => {
+    const arr = obj.testsuite.testcase;
+
+    if (arr.length !== 0) {
+        const result = arr.filter((el) => {
+            return el.failure !== undefined;
+        });
+        return (result.length !== 0) ? result : undefined;
+    }
+};
+
+parseTestReport();
