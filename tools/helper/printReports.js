ed9b92ea401cddb1eeeba882d209d6354b1058be
zhenmichael <michaelzhen@microsoft.com>
2022-11-29 09:56:42 -0800

    wip

--- tools/helper/printReports.js
+++ tools/helper/printReports.js
@@ -1,30 +1,26 @@
 import * as fs from "fs";
-import * as path from "path";
+import * as glob from "glob";
 
-var __dirname = process.argv.slice(2)[0];
+var directory = process.argv.slice(2)[0];
 
-// const printFiles = () => {
-//     var files = fs.readdirSync(directory);
-//     console.log(files)
-// }
+const printFiles = () => {
+    var files = fs.readdirSync(directory);
+    console.log(files)
+
+    files.forEach(file => {
+        getDirectories(file, function (err, res) {
+            if (err) {
+              console.log('Error', err);
+            } else {
+              console.log(res);
+            }
+          });
+    })
+}
 
 
-var directories = fs.readdirSync(__dirname).filter(function (file) {
-    return fs.statSync(path.join(__dirname, file)).isDirectory();
-});
+var getDirectories = function (src, callback) {
+  glob(src + '/**/*', callback);
+};
 
-var model_files = [];
-directories.forEach(function (value, index, array) {
-    var current_model_files = fs.readdirSync(path.join(__dirname, value)).filter(function (file) {
-        return file.endsWith('junit-report.xml');
-    }).map(function (file) {
-        return path.join(__dirname, value, file);
-    });
-
-    model_files = model_files.concat(current_model_files);
-});
-
-// Do something with model_files here ...
-console.log(model_files);
-
-// printFiles();
+printFiles();
