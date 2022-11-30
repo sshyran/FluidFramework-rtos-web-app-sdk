2ca79d09d4eb15d360afbb8acb7133b20f14e48e
zhenmichael <michaelzhen@microsoft.com>
2022-11-30 13:37:56 -0800

    wip

--- tools/helper/printReports.js
+++ tools/helper/printReports.js
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
+import * as parser from "xml2js";
 
 var directory = process.argv.slice(2)[0];
 
@@ -17,18 +18,37 @@ const getFilesRecursively = (directory) => {
     }
   }
 
-// const printFiles = () => {
-//     var files = fs.readdirSync(directory);
-//     console.log(files)
-
-//     files.forEach(file => {
-//         const filePath = "C:/Users/michaelzhen/Documents/GitHub/microsoftFluidFramework/tools/" + file;
-//         if(fs.statSync(filePath).isDirectory()){
-//             getFilesRecursively(filePath);
-//         }
-//     })
-// }
-
 getFilesRecursively(directory);
 
 console.log(files)
+
+const parseTestReport = (filename) => {
+    fs.readFile(filename,  'utf8', (err, data) => {
+        let failedTests;
+        parser.parseString(data, { mergeAttrs: true }, (err, res) => {
+            failedTests = findFailedTests(res);
+        })
+
+        failedTests?.forEach((test) => {
+            console.log(test.name[0])
+            console.log(test.failure[0]);
+        });
+
+        return failedTests;
+    });
+}
+
+const findFailedTests = (obj) => {
+    const arr = obj.testsuite.testcase;
+
+    if (arr.length !== 0) {
+        const result = arr.filter((el) => {
+            return el.failure !== undefined;
+        });
+        return (result.length !== 0) ? result : undefined;
+    }
+};
+
+files.forEach((filename) => {
+    parseTestReport(filename);
+})
